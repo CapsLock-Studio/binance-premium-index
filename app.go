fac7d5ebd04411936abb4b33170b4b37d6b9cb5f
Michael Kuan <michael34435@capslock.tw>
2022-09-30 02:49:20 +0800

    feat: add recovery mode and new functions

--- app.go
+++ app.go
@@ -35,6 +35,7 @@ func extractPremiumIndex(premium []models.BinancePremium, currency string) (valu
 func main() {
 	route := gin.Default()
 
+	route.Use(gin.Recovery())
 	route.GET("/", func(ctx *gin.Context) {
 		res, err := http.Get("https://fapi.binance.com/fapi/v1/premiumIndex")
 		if err != nil {
--- models/BinanceHedge.go
+++ models/BinanceHedge.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"strings"
+
+	"github.com/shopspring/decimal"
+)
+
 type BinanceHedge struct {
 	Symbol         string           `json:"symbol"`
 	FundingRateGap float64          `json:"fundingRateGap"`
@@ -7,4 +13,16 @@ type BinanceHedge struct {
 	ProfitForTimes int64            `json:"profitForTimes"`
 	Direction      bool             `json:"direction"`
 	Index          []BinancePremium `json:"index"`
+}
+
+func (h *BinanceHedge) GetPrice(currency string) (price float64) {
+	for _, i := range h.Index {
+		if strings.HasSuffix(i.Symbol, currency) {
+			markPrice, _ := decimal.NewFromString(i.MarkPrice)
+			price, _ = markPrice.Float64()
+			break
+		}
+	}
+
+	return
 }
